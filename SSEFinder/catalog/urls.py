7ae229b4cb591bbc099024adcbfc7c193744ba79
u3557110 <47244396+u3557110@users.noreply.github.com>
2021-05-05 16:03:43 +0800

    update and delete case from event

--- SSEFinder/catalog/urls.py
+++ SSEFinder/catalog/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('case/create/', views.CaseCreate.as_view(), name='case-create'),
     path('case/<int:pk>/update/', views.CaseUpdate.as_view(), name='case-update'),
     path('case/<int:pk>/delete/', views.CaseDelete.as_view(), name='case-delete'),
+    path('event/case/<int:pk>/update/', views.CaseUpdate.as_view(), name='case-update'),
+    path('event/case/<int:pk>/delete/', views.CaseDelete.as_view(), name='case-delete'),
     # location
     path('locations/', views.LocationListView.as_view(), name='locations'),
     path('location/<int:pk>', views.LocationDetailView.as_view(), name='location-detail'),
@@ -19,6 +21,7 @@ urlpatterns = [
     path('event/<int:pk>', views.EventDetailView.as_view(), name='event-detail'),
     path('events/create/', views.EventCreate.as_view(), name='event-create'),
     path('event/<int:pk>/update/', views.EventUpdate.as_view(), name='event-update'),
+    path('case/event/<int:pk>/update/', views.EventUpdate.as_view(), name='event-update'),
     path('event/<int:pk>/delete/', views.EventDelete.as_view(), name='event-delete'),
     # attend
     path('attend/create/', views.AddAttend, name='attend-create'),
